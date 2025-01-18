65f2bc09ee1e4bca5bd9a140cbe3dce74e3006a7
Pawel Kowalik <pawel-kow@users.noreply.github.com>
2025-01-18 14:43:56 +0000

    Added cron script

--- prepare_system_ubuntu.sh
+++ prepare_system_ubuntu.sh
@@ -8,3 +8,6 @@ groupadd --gid 953 --system pdns
 adduser --uid 953 --system --no-create-home --ingroup pdns pdns
 groupadd --gid 1002 --system pdamysql
 adduser --uid 1002 --system --no-create-home --ingroup pda pdamysql
+
+#add cron job 
+#10 0 * * * cd /root/Templates && ((git add . && git stash && git pull origin master --rebase -X theirs && (git stash pop || true) && (git restore --staged . || true) && (git stash clear || true)) || true) && chown pda:pda * &> /dev/null
